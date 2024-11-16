7ce34d30672702a3fc2ab979c434417cc831b634
横田昴輝 <kokiyokotakoki@gmail.com>
2024-11-16 23:37:40 +0900

    add test

---
+++ my-app/const/common.ts
@@ -0,0 +1,24 @@
+import { OptionType } from "../type/common";
+
+export const FRAMEWORKS: OptionType[] = [
+    {
+      value: 1,
+      label: "Next.js"
+    },
+    {
+      value: 2,
+      label: "SvelteKit"
+    },
+    {
+      value: 3,
+      label: "Nuxt.js"
+    },
+    {
+      value: 4,
+      label: "Remix"
+    },
+    {
+      value: 5,
+      label: "Astro"
+    }
+  ];
